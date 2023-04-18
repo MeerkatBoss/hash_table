85ef0558c28d3808d8db116d54b214fc6aba83f8
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-04-18 11:52:24 +0300

    Initial commit

---
+++ src/hash_table/hash_table.h
@@ -0,0 +1,17 @@
+/**
+ * @file hash_table.h
+ * @author MeerkatBoss (solodovnikov.ia@phystech.edu)
+ * 
+ * @brief
+ *
+ * @version 0.1
+ * @date 2023-04-18
+ *
+ * @copyright Copyright MeerkatBoss (c) 2023
+ */
+#ifndef __HASH_TABLE_H
+#define __HASH_TABLE_H
+
+
+
+#endif /* hash_table.h */
---
+++ src/hash_table/hashes/hash_functions.h
@@ -0,0 +1,17 @@
+/**
+ * @file hash_functions.h
+ * @author MeerkatBoss (solodovnikov.ia@phystech.edu)
+ * 
+ * @brief
+ *
+ * @version 0.1
+ * @date 2023-04-18
+ *
+ * @copyright Copyright MeerkatBoss (c) 2023
+ */
+#ifndef __HASH_FUNCTIONS_H
+#define __HASH_FUNCTIONS_H
+
+
+
+#endif /* hash_functions.h */
